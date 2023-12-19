2116f0b5bf46f63fc35e4b4890d5db7a484abaf3
theo-baccam <theo.baccam@laplateforme.io>
2023-12-19 15:52:29 +0100

    fonction pour calculer grilles, pommes placées aléatoirement sur grille

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from string import ascii_uppercase
 from functools import partial
+from random import choice
 
 import pygame
 
@@ -10,29 +12,35 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 BACKGROUND = (64, 128, 32)
 FOREGROUND = (16, 64, 16)
 
-
 clock = pygame.time.Clock()
 MAX_FPS = 3
 running = True
 
-border_top = pygame.Rect((0, 0), (608, 32))
-border_right = pygame.Rect((608, 0), (32, 448))
-border_bottom = pygame.Rect((32, 448), (608, 32))
-border_left = pygame.Rect((0, 32), (32, 448))
 
-border_list = [border_top, border_right, border_bottom, border_left]
+def calculate_grid():
+    grid_coordinate = {}
+    GRID_WIDTH = 20
+    GRID_HEIGHT = 15
+    for index, letter in enumerate(ascii_uppercase[:GRID_HEIGHT]):
+        for num in range(GRID_WIDTH):
+            name = f"{letter}{num + 1}"
+            grid_coordinate[name] = (num * 32, index * 32)
+    return grid_coordinate
 
-snake_head_x, snake_head_y = (
-    320,
-    224,
-)
-snake_head = pygame.Rect((snake_head_x, snake_head_y), (32, 32))
 
-apple_x, apple_y = (
-    64,
-    64,
-)
-apple = pygame.Rect((apple_x, apple_y), (32, 32))
+def choose_random_coordinate(grid_coordinate, snake_positions):
+    while True:
+        random_coordinate = choice(list(grid_coordinate.values()))
+        if (
+            (random_coordinate[0] == 0) or
+            (random_coordinate[0] == 608) or
+            (random_coordinate[1] == 0) or
+            (random_coordinate[1] == 448)
+        ):
+            continue
+        elif random_coordinate in snake_positions:
+            continue
+        return random_coordinate
 
 
 def move(increment_x, increment_y):
@@ -70,9 +78,7 @@ def border_collision(snake_head, border_list):
 
 def apple_collision(snake_head, apple):
     if snake_head.colliderect(apple):
-        return 1
-    else:
-        return 0
+        return True
 
 
 def calculate_snake_positions(snake_positions, snake_length, snake_head):
@@ -87,18 +93,28 @@ def render_snake(snake_positions, screen, FOREGROUND):
         pygame.draw.rect(screen, FOREGROUND, snake_cell)
 
 
+grid_coordinate = calculate_grid()
+
+border_top = pygame.Rect(grid_coordinate["A1"], (608, 32))
+border_right = pygame.Rect(grid_coordinate["A20"], (32, 448))
+border_bottom = pygame.Rect(grid_coordinate["O2"], (608, 32))
+border_left = pygame.Rect(grid_coordinate["B1"], (32, 448))
+border_list = [border_top, border_right, border_bottom, border_left]
+
+snake_head_x, snake_head_y = grid_coordinate["H11"]
+snake_head = pygame.Rect((snake_head_x, snake_head_y), (32, 32))
+
+
 direction = "right"
 new_direction = ""
 snake_length = 3
 snake_positions = []
 
+random_coordinate = choose_random_coordinate(grid_coordinate, snake_positions)
+apple_x, apple_y = random_coordinate
+apple = pygame.Rect((apple_x, apple_y), (32, 32))
+
 while running:
-    screen.fill(BACKGROUND)
-
-    draw_border(screen, FOREGROUND, border_list)
-
-    pygame.draw.rect(screen, FOREGROUND, apple)
-
     for event in pygame.event.get():
         if event.type != pygame.KEYDOWN:
             continue
@@ -109,16 +125,23 @@ while running:
             new_direction = key_name
         if not is_opposite_direction(direction, new_direction):
             direction = new_direction
+
     snake_head = movement_keys[direction]()
+    calculate_snake_positions(snake_positions, snake_length, snake_head)
+
+    if apple_collision(snake_head, apple):
+        snake_length += 1
+        apple[0], apple[1] = choose_random_coordinate(grid_coordinate, snake_positions)
+
+    screen.fill(BACKGROUND)
+
+    draw_border(screen, FOREGROUND, border_list)
+    pygame.draw.rect(screen, FOREGROUND, apple)
+    render_snake(snake_positions, screen, FOREGROUND)
 
     if border_collision(snake_head, border_list):
         running = False
-
-    snake_length += apple_collision(snake_head, apple)
-
-    calculate_snake_positions(snake_positions, snake_length, snake_head)
-
-    render_snake(snake_positions, screen, FOREGROUND)
+        break
 
     pygame.display.flip()
     clock.tick(MAX_FPS)
